dbbf5fe330f5a6a250a0d6cfd773c5d5d73e904c
Materist <38249437+Materist@users.noreply.github.com>
2019-03-13 23:01:22 +0100

    Update SoMuchLaserz.vb

--- SoMuchLaserz.vb
+++ SoMuchLaserz.vb
@@ -1,49 +1,57 @@
-OK=0
-KOL=1
+Global MAKS
+MAKS=0
 
-Gosub SPLASHSCREEN
+SPLASHSCREEN
 
-MAINMENU:
-   Load "AMOS_BANK:AMOS_Music/Music_Bumioh.ABK",15
-   Load Iff "asm:GrafikiLaserz/LOADING.PIC",5
-   Bank Swap 3,15
-   Music 1
-   Do 
-      If Fire(1)
-         Erase All 
-         End 
-      End If 
-   Loop 
+Procedure CHECKFIRE
+   If Fire(1)
+      BUTFIRE=1
+   Else 
+      BUTFIRE=0
+   End If 
+End Proc[BUTFIRE]
 
-SPLASHSCREEN:
-   Do 
-      If OK=0
-         Load Iff "asm:GrafikiLaserz/INTRO.PIC",5
-         Load "asm:GrafikiLaserz/MegaMen/bobek",14
-         Channel 1 To Bob 1
-         Bob 1,200,200,1
-         Anim 1,"(1,60) (2,15)L"
-         Anim On 
-         Load "AMOS_BANK:AMOS_Music/Music_Draconus.ABK",3
-         Music 1
-         OK=1
-      End If 
 
-      If KOL=16
-         KOL=1
-      End If 
+Procedure KOLTEXT[KOL,LOWKOL,HIGHKOL,X,Y,MSG$]
 
-      Ink KOL,7,
-      Text 80,150,"PRESS FIRE BUTTON"
+   If MAKS=0
       KOL=KOL+1
+   Else 
+      KOL=KOL-1
+   End If 
 
-      If Fire(1)
-         Erase 3
-         Erase 5
-         OK=0
-         Gosub MAINMENU
-      End If 
+   If KOL=HIGHKOL
+      MAKS=1
+   End If 
 
-      Wait 2
-   Loop 
+   If KOL=LOWKOL
+      MAKS=0
+   End If 
 
+   Ink KOL,7,
+   Wait Vbl 
+   Wait Vbl 
+   Text X,Y,MSG$
+
+
+End Proc[KOL]
+
+Procedure SPLASHSCREEN
+   BUTFIRE=0
+   KOL=0
+   Load Iff "asm:GrafikiLaserz/INTRO.PIC",5
+   Load "asm:GrafikiLaserz/MegaMen/bobek",14
+   Channel 1 To Bob 1
+   Bob 1,200,200,1
+   Anim 1,"(1,60) (2,15)L"
+   Anim On 
+   Load "AMOS_BANK:AMOS_Music/Music_Draconus.ABK",3
+   Music 1
+   Repeat 
+      CHECKFIRE
+      BUTFIRE=Param
+      KOLTEXT[KOL,0,16,80,150,"PRESS START BUTTON"]
+      KOL=Param
+   Until BUTFIRE=1
+   Erase All 
+End Proc
